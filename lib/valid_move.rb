454fd01c58321e916da62d872dfdb2b29ff2f2f2
Marilexia Faith Acelajado <faithacelajado@gmail.com>
2018-06-03 19:55:46 +0000

    automatically backed up by learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,24 +1,26 @@
 
-def input_to_index(input)
-  input.to_i - 1
-end
-
-def user_input(input1)
-  if input1.between?(0, 8)
-    valid
-  else
-    invalid
-end
+# def input_to_index(input)
+#   input.to_i - 1
+# end
+#
+# def user_input(input1)
+#   if input1.between?(0, 8)
+#     valid
+#   else
+#     invalid
+# end
 
 # code your #valid_move? method here
 
 def valid_move?(board, index)
   user_input = input.to_i - 1
 
-  if position_taken?
-
-  if board[index] =
+  if position_taken?(board, index) && user_input.between?(0,8)
     true
+  else
+    false
+  # if board[index] =
+  #   true
 
   end
 
