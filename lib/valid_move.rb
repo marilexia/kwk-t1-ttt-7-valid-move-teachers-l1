1b656d7c1da2ce3c4bc2b668f7584bdad4dfa717
Marilexia Faith Acelajado <faithacelajado@gmail.com>
2018-06-03 20:01:29 +0000

    automatically backed up by learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -4,16 +4,12 @@
 def valid_move?(board, index)
 
   if index.between?(0,8) && position_taken?(board, index) == false
-    
     true
 
   else
     false
-  # if board[index] =
-  #   true
-
+    
   end
-
 end
 
 
